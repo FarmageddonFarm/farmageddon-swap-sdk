30b965a499de688bbf650748613d63c790b3adfc
topleft1562 <87454455+topleft1562@users.noreply.github.com>
2022-01-26 08:19:23 -0600

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import warning from 'tiny-warning'
 import JSBI from 'jsbi'
 import { getAddress } from '@ethersproject/address'
 
-import { BigintIsh, ZERO, ONE, TWO, THREE, SolidityType, SOLIDITY_TYPE_MAXIMA, FACTORY_DONK, DONK_CODE_HASH, FACTORY_BABY, BABY_CODE_HASH, APE_CODE_HASH, FACTORY_APE, FACTORY_GUITAR, GUITAR_CODE_HASH, FACTORY_AUTOSHARK, AUTOSHARK_CODE_HASH,FACTORY_BISWAP, BISWAP_CODE_HASH } from './constants'
+import { BigintIsh, ZERO, ONE, TWO, THREE, SolidityType, SOLIDITY_TYPE_MAXIMA, FACTORY_DONK, DONK_CODE_HASH, FACTORY_BABY, BABY_CODE_HASH, APE_CODE_HASH, FACTORY_APE, FACTORY_GUITAR, GUITAR_CODE_HASH, FACTORY_AUTOSHARK, AUTOSHARK_CODE_HASH, FACTORY_BISWAP, BISWAP_CODE_HASH, FACTORY_YUKLA, YUKLA_CODE_HASH } from './constants'
 import { FACTORY_ADDRESS, INIT_CODE_HASH } from '.'
 
 export function validateSolidityTypeInstance(value: JSBI, solidityType: SolidityType): void {
@@ -84,9 +84,11 @@ export function sortedInsert<T>(items: T[], add: T, maxSize: number, comparator:
 
 export function getInitHash(factory: string): string {
   switch(factory) {
+    case FACTORY_YUKLA:
+      return YUKLA_CODE_HASH;
     case FACTORY_DONK:
       return DONK_CODE_HASH;
-      case FACTORY_BISWAP:
+    case FACTORY_BISWAP:
       return BISWAP_CODE_HASH;
     case FACTORY_AUTOSHARK:
       return AUTOSHARK_CODE_HASH;
